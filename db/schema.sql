642451ace4214da25ef79326c80f0d3ce10d483b
funnelbit <numberwithint@gmail.com>
2019-11-05 15:56:09 +0900

    ユーザーに関連するテーブルを作りましょう

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,23 @@
--- CREATE TABLE user (
+CREATE TABLE user (
+  `id` BIGINT UNSINGNED NOT NULL,
+  `name` VARBINARY(32) NOT NULL,
+  `password_hash` VARBINARY(254) NOT NULL,
 
--- CREATE TABLE user_session (
+  `created_at` DATETIME(6) NOT NULL,
+  `updated_at` DATETIME(6) NOT NULL,
 
+  PRIMARY KEY (id),
+  UNIQUE KEY (name),
+
+  KEY (created_at),
+  KEY (updated_at),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE user_session (
+  `id` BIGINT UNSINGNED NOT NULL,
+  `user_id` BIGINT UNSINGNED NOT NULL,
+  `expred_at` BIGINT UNSINGNED NOT NULL,
+
+  PRIMARY KEY (id),
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
