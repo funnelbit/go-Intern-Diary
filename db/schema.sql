242017479af5a271dc18a9cb860d7eaa4b441c13
funnelbit <numberwithint@gmail.com>
2019-11-08 11:47:11 +0900

    diary と article のスキーマ作った

--- db/schema.sql
+++ db/schema.sql
@@ -24,3 +24,33 @@ CREATE TABLE user_session (
 
   PRIMARY KEY (token)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE diary (
+  `id` BIGINT UNSIGNED NOT NULL,
+  `name` VARBINARY(64) NOT NULL,
+  `user_id` BIGINT UNSIGNED NOT NULL,
+  
+  PRIMARY KEY (id),
+
+  `created_at` DATETIME(6) NOT NULL,
+  `updated_at` DATETIME(6) NOT NULL,
+  
+  KEY (user_id),
+  KEY (created_at),
+  KEY (updated_at)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE article (
+  `id` BIGINT UNSIGNED NOT NULL,
+  `diary_id` BIGINT UNSIGNED NOT NULL,
+  `body` LONGTEXT,
+
+  PRIMARY KEY (id),
+
+  `created_at` DATETIME(6) NOT NULL,
+  `updated_at` DATETIME(6) NOT NULL,
+  
+  KEY (diary_id),
+  KEY (created_at),
+  KEY (updated_at)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
