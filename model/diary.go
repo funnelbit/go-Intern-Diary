56d259404d581d205a43975bd541727cbc1b75c7
funnelbit <numberwithint@gmail.com>
2019-11-08 18:55:16 +0900

    diary モデル

---
+++ model/diary.go
@@ -0,0 +1,12 @@
+package model
+
+import (
+	"time"
+)
+
+type Diary struct {
+	ID	 uint64 `db:"id"`
+	Name string `db:"name"`
+	UserID uint64 `db:"user_id"`
+	Created time.Time `db:"created_at"`
+}
